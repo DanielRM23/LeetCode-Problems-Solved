4ce62efde1ebe0636aca07b65d73850899231e40
DanielRM23 <danielrojomata@gmail.com>
2026-03-31 12:51:59 -0600

    Roman to Integer, problem 13, comments added

---
+++ romanToInt.py
@@ -0,0 +1,60 @@
+# Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+
+# Symbol       Value
+# I             1
+# V             5
+# X             10
+# L             50
+# C             100
+# D             500
+# M             1000
+# For example, 2 is written as II in Roman numeral, just two ones added together. 12 is written as XII, which is simply X + II. The number 27 is written as XXVII, which is XX + V + II.
+
+# Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII. Instead, the number four is written as IV. Because the one is before the five we subtract it making four. The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
+
+# I can be placed before V (5) and X (10) to make 4 and 9.
+# X can be placed before L (50) and C (100) to make 40 and 90.
+# C can be placed before D (500) and M (1000) to make 400 and 900.
+# Given a roman numeral, convert it to an integer.
+
+
+def romanToInt(s):
+    # Use a hash table to store the values of the letters
+    hast_table = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+
+    # Length of the string
+    n = len(s)
+    # Use two variables to sum and to substract
+    adition = 0
+    rest = 0
+
+    # Iterate over the string but don't take the last one, but this is added at the end
+    for i in range(n - 1):
+        # Sum all the letters (the correspondig value of each one)
+        adition += hast_table[s[i]]
+
+        # But for every restriction we need to substract the values
+        if s[i] == "I" and s[i + 1] == "V":
+            rest -= 2
+        elif s[i] == "I" and s[i + 1] == "X":
+            rest -= 2
+        elif s[i] == "X" and s[i + 1] == "L":
+            rest -= 20
+        elif s[i] == "X" and s[i + 1] == "C":
+            rest -= 20
+        elif s[i] == "C" and s[i + 1] == "D":
+            rest -= 200
+        elif s[i] == "C" and s[i + 1] == "M":
+            rest -= 200
+
+    # At the end, sum everything and substract the rest
+    # Don't forget to add the last value
+    result = adition + rest + hast_table[s[-1]]
+
+    return result
+
+
+s = "MCMXCIV"
+s2 = "VIII"
+s3 = "D"
+print(romanToInt(s3))
