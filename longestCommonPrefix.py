1eb6a35fc923f1224e395966d23c08a5b119f0ab
DanielRM23 <danielrojomata@gmail.com>
2026-04-11 23:38:52 -0600

    Longest Substring Without Repeating Characters, problem 3

--- longestCommonPrefix.py
+++ longestCommonPrefix.py
@@ -3,7 +3,6 @@
 # Write a function to find the longest common prefix string amongst an array of strings.
 # If there is no common prefix, return an empty string "".
 
-
 def longestCommonPrefix(strs):
     # Check if there are no strings
     if len(strs) == 0:
